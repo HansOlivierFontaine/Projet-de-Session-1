b3c65ebb3e547d516bad167bd4ce8b376d468467
legroswil02 <pelw3101@usherbrooke.ca>
2018-10-10 18:35:38 -0400

    Bonjour

--- Projet_Parcour_21/src/main.cpp
+++ Projet_Parcour_21/src/main.cpp
@@ -18,10 +18,10 @@ Variables globales et defines
 **************************************************************************** */
 
 double speedGauche = 0.6;
-double speedDroite = 0.6;
+double speedDroite = 0.605;
 int delayEntreMouvement = 200;
-int nbCycle =1 ;
-#define comptePulseTheo 3200 ;
+int nbCycle = 1;
+#define comptePulseTheo 3200;
 //#define PI 3.1415926
 // -> defines...
 // L'ensemble des fonctions y ont acces
@@ -65,7 +65,7 @@ void Decceleration()
 }
 
 void PIDParMoteur()
-{ 
+{
     int comptePulseG = 0;
     int comptePulseD = 0;
     int comptePulseGTot = 0;
@@ -76,36 +76,34 @@ void PIDParMoteur()
     float compteErreurD = 0;
     float compteErreurG = 0;
 
-    int comptePulseTheoCycle= 0 ;
-     
+    int comptePulseTheoCycle = 0;
+
     // 1 seconde
-    
-    comptePulseD = ENCODER_Read(RIGHT) - (nbCycle*3200);
-    comptePulseG = ENCODER_Read(LEFT) - (nbCycle*3200);
-    
-    comptePulseDTot+= comptePulseD;
-    comptePulseGTot+= comptePulseG;
+
+    comptePulseD = ENCODER_Read(RIGHT) - (nbCycle * 3200);
+    comptePulseG = ENCODER_Read(LEFT) - (nbCycle * 3200);
+
+    comptePulseDTot += comptePulseD;
+    comptePulseGTot += comptePulseG;
 
     delay(10);
 
     compteErreurD = comptePulseTheo - comptePulseD;
     compteErreurG = comptePulseTheo - comptePulseG;
 
-    comptePulseTheoCycle = nbCycle*comptePulseTheo;
-
+    comptePulseTheoCycle = nbCycle * comptePulseTheo;
 
     compteErreurDTot = comptePulseTheoCycle - comptePulseDTot;
-    speedDroite+= (compteErreurDTot * 0.00002)+(compteErreurD * 0.0001) ;
-    
+    speedDroite += (compteErreurDTot * 0.00002) + (compteErreurD * 0.0001);
+
     compteErreurGTot = comptePulseTheoCycle - comptePulseGTot;
-    speedGauche+= (compteErreurGTot * 0.00002)+(compteErreurG * 0.0001) ;
- 
+    speedGauche += (compteErreurGTot * 0.00002) + (compteErreurG * 0.0001);
 
     nbCycle++;
 }
 void PIDBasic()
 {
-    
+
     int comptePulseGauche = 0;
     int comptePulseDroit = 0;
     float rapportErreur = 0;
@@ -115,12 +113,12 @@ void PIDBasic()
 
     rapportErreur = (comptePulseDroit - comptePulseGauche);
 
-    float KI = (nbCycle * 5000 ) - comptePulseGauche ; 
+    float KI = (nbCycle * 5000) - comptePulseGauche;
     Serial.print("nbCylce  =");
     Serial.println(nbCycle, 5);
     Serial.print("AVANT KI  =");
     Serial.println(KI, 5);
-    KI = KI * 0.00002; 
+    KI = KI * 0.00002;
 
     speedGauche += (rapportErreur * 0.0001);
 
@@ -146,76 +144,28 @@ void Avancer_Cm(int distance)
 
     ENCODER_Reset(RIGHT);
     ENCODER_Reset(LEFT);
-    speedGauche = 0.625;
-    speedDroite = 0.6;
+    speedGauche = 0.6;
+    speedDroite = 0.605;
     Acceleration();
     nbCycle = 0;
     int temps = 0;
-    int prochainCycle= 3200;
+    int prochainCycle = 3200;
 
     while (Compteur < ComptePulse)
     {
-       
-       if(Compteur >= prochainCycle)
+
+        if (Compteur >= prochainCycle)
         {
             prochainCycle = Compteur + 3200;
             PIDParMoteur();
             temps = 0;
         }
-        
-        
-        delay(5);           
+
+        delay(5);
 
         Compteur = ENCODER_Read(RIGHT);
-       //PIDBasic();
     }
-      Decceleration();
-
-}
-void Avancer200()
-{
-    long Compteur = 0;
-
-    float circonference = 3.1415926 * 7.5;
-    long PulseParCm = 32000 / circonference;
-    long ComptePulse = (PulseParCm * (200));
-    ComptePulse = ComptePulse / 10;
-
-    ENCODER_Reset(RIGHT);
-    ENCODER_Reset(LEFT);
-    speedGauche = 0.62;
-    speedDroite = 0.6;
-    Acceleration();
-    nbCycle = 0;
-    int temps = 0;
-    int prochainCycle= 3200;
-
-    while (Compteur < ComptePulse)
-    {
-       
-       if(Compteur >= prochainCycle)
-        {
-            prochainCycle = Compteur + 3200;
-            PIDParMoteur();
-            temps = 0;
-        }
-        
-        
-        delay(5);           
-
-        Compteur = ENCODER_Read(RIGHT);
-       //PIDBasic();
-    }
-    
-    MOTOR_SetSpeed(LEFT, 0.4);
-    MOTOR_SetSpeed(RIGHT, 0.4);
-    delay(50);
-    MOTOR_SetSpeed(LEFT, 0.2);
-    MOTOR_SetSpeed(RIGHT, 0.2);
-    delay(50);
-    MOTOR_SetSpeed(LEFT, 0);
-    MOTOR_SetSpeed(RIGHT, 0);
-
+    Decceleration();
 }
 
 void Tourne_Pulse(int direction, float NbPulse)
@@ -228,7 +178,7 @@ void Tourne_Pulse(int direction, float NbPulse)
         MOTOR_SetSpeed(RIGHT, 0);
         MOTOR_SetSpeed(LEFT, 0.6);
         comptePulse = 0;
-        while (comptePulse <= (NbPulse*3200))
+        while (comptePulse <= (NbPulse * 3200))
         {
             comptePulse = ENCODER_Read(LEFT);
             delay(10);
@@ -241,73 +191,7 @@ void Tourne_Pulse(int direction, float NbPulse)
         MOTOR_SetSpeed(RIGHT, 0.6);
         MOTOR_SetSpeed(LEFT, 0);
         comptePulse = 0;
-        while (comptePulse <= (NbPulse*3200))
-        {
-            comptePulse = ENCODER_Read(RIGHT);
-            delay(10);
-        }
-        MOTOR_SetSpeed(LEFT, 0);
-        MOTOR_SetSpeed(RIGHT, 0);
-    }
-}
-
-void Tourne45(int direction)
-{
-    int comptePulse;
-    ENCODER_Reset(LEFT);
-    ENCODER_Reset(RIGHT);
-    if (direction == RIGHT)
-    {
-        MOTOR_SetSpeed(RIGHT, 0);
-        MOTOR_SetSpeed(LEFT, 0.6);
-        comptePulse = 0;
-        while (comptePulse <= (3200 * 0.6))
-        {
-            comptePulse = ENCODER_Read(LEFT);
-            delay(10);
-        }
-        MOTOR_SetSpeed(LEFT, 0);
-        MOTOR_SetSpeed(RIGHT, 0);
-    }
-    else if (direction == LEFT)
-    {
-        MOTOR_SetSpeed(RIGHT, 0.6);
-        MOTOR_SetSpeed(LEFT, 0);
-        comptePulse = 0;
-        while (comptePulse <= (3200 * 0.6))
-        {
-            comptePulse = ENCODER_Read(RIGHT);
-            delay(10);
-        }
-        MOTOR_SetSpeed(LEFT, 0);
-        MOTOR_SetSpeed(RIGHT, 0);
-    }
-}
-
-void Tourne90(int direction)
-{
-    int comptePulse;
-    ENCODER_Reset(LEFT);
-    ENCODER_Reset(RIGHT);
-    if (direction == RIGHT)
-    {
-        MOTOR_SetSpeed(RIGHT, 0);
-        MOTOR_SetSpeed(LEFT, 0.6);
-        comptePulse = 0;
-        while (comptePulse <= (3200 * 1.18))
-        {
-            comptePulse = ENCODER_Read(LEFT);
-            delay(10);
-        }
-        MOTOR_SetSpeed(LEFT, 0);
-        MOTOR_SetSpeed(RIGHT, 0);
-    }
-    else if (direction == LEFT)
-    {
-        MOTOR_SetSpeed(RIGHT, 0.6);
-        MOTOR_SetSpeed(LEFT, 0);
-        comptePulse = 0;
-        while (comptePulse <= (3200 * 1.18))
+        while (comptePulse <= (NbPulse * 3200))
         {
             comptePulse = ENCODER_Read(RIGHT);
             delay(10);
@@ -342,48 +226,102 @@ void loop()
     delay(1500);
 
     //Pour le robot 21 seulement (moteurs valeur égaux)
-     Avancer200();
+    //Avancer200();
     //Pour le robot 20  (moteur gauche 0.025 plus fort)
-   //Avancer_Cm(200);
+    Avancer_Cm(200);
 
     delay(500);
     //DEBUT DU U
-    
-    Tourne_Pulse(LEFT , 1.18);
-    
+
+    Tourne_Pulse(LEFT, 1.18);
+
     delay(delayEntreMouvement);
     Avancer_Cm(30);
     delay(delayEntreMouvement);
 
     //LIGNE BASSE U
-    Tourne_Pulse(RIGHT , 1.18);
+    Tourne_Pulse(RIGHT, 1.18);
 
     delay(delayEntreMouvement);
     Avancer_Cm(20);
     delay(delayEntreMouvement);
 
-
-    Tourne_Pulse(RIGHT , 1.18);
+    Tourne_Pulse(RIGHT, 1.18);
     delay(delayEntreMouvement);
     Avancer_Cm(15);
-    //FIN DU U 
+    //FIN DU U
     delay(delayEntreMouvement);
 
-    Tourne_Pulse(LEFT , 0.8);
+    //Gauche
+    Tourne_Pulse(LEFT, 0.8);
 
     delay(delayEntreMouvement);
-    Avancer_Cm(58);
-    delay(delayEntreMouvement);
 
-    Tourne_Pulse(LEFT , 0.8);
-
-    delay(delayEntreMouvement);
     Avancer_Cm(60);
     delay(delayEntreMouvement);
-    
-    Tourne_Pulse(RIGHT , 0.45);
-
+    //Gauche
+    Tourne_Pulse(LEFT, 0.95);
     delay(delayEntreMouvement);
-    Avancer_Cm(85); 
-  
+
+    Avancer_Cm(60);
+    delay(delayEntreMouvement);
+
+    Tourne_Pulse(RIGHT, 0.65);
+    delay(delayEntreMouvement);
+
+    Avancer_Cm(100);
+
+    //Retour
+
+    delay(1000);
+
+    MOTOR_SetSpeed(RIGHT, -0.6);
+    MOTOR_SetSpeed(LEFT, 0.6);
+
+    delay(675);
+
+    MOTOR_SetSpeed(RIGHT, 0);
+    MOTOR_SetSpeed(LEFT, 0);
+    delay(60);
+
+    Avancer_Cm(100);
+    delay(60);
+
+    Tourne_Pulse(LEFT, 0.6);
+    delay(60);
+
+    Avancer_Cm(65);
+    delay(60);
+
+    Tourne_Pulse(RIGHT, 0.8);
+
+    delay(60);
+
+    Avancer_Cm(60);
+    delay(60);
+
+    Tourne_Pulse(RIGHT, 0.8);
+    delay(60);
+
+    Avancer_Cm(15);
+    delay(60);
+
+    Tourne_Pulse(LEFT, 1.18);
+    delay(60);
+
+    Avancer_Cm(20);
+    delay(60);
+
+    Tourne_Pulse(LEFT, 1.18);
+    delay(60);
+
+    Avancer_Cm(30);
+    delay(60);
+
+    Tourne_Pulse(RIGHT, 1.18);
+
+    Avancer_Cm(215);
+
+    delay(30000);
+
 }
