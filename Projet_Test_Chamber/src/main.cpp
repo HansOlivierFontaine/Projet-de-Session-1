9c6baae179e89572d5c5d1aabcb7bb6267068527
Tommy Dufour <tommydufour1@gmail.com>
2018-10-10 14:24:27 -0400

    Inclu PIDparMoteur

--- Projet_Test_Chamber/src/main.cpp
+++ Projet_Test_Chamber/src/main.cpp
@@ -20,7 +20,8 @@ Variables globales et defines
 double speedGauche = 0.625;
 double speedDroite = 0.6;
 int delayEntreMouvement = 200;
-int nbCycle =0 ;
+int nbCycle =1 ;
+#define comptePulseTheo 3200 ;
 //#define PI 3.1415926
 // -> defines...
 // L'ensemble des fonctions y ont acces
@@ -38,13 +39,13 @@ void Acceleration()
     MOTOR_SetSpeed(RIGHT, speedDroite * 0.1);
     delay(50);
     MOTOR_SetSpeed(LEFT, speedGauche * 0.3);
-    MOTOR_SetSpeed(RIGHT, speedDroite * 0.3);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.2);
     delay(50);
     MOTOR_SetSpeed(LEFT, speedGauche * 0.5);
-    MOTOR_SetSpeed(RIGHT, speedDroite * 0.5);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.4);
     delay(50);
     MOTOR_SetSpeed(LEFT, speedGauche * 0.7);
-    MOTOR_SetSpeed(RIGHT, speedDroite * 0.7);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.6);
     delay(50);
     MOTOR_SetSpeed(LEFT, speedGauche * 1);
     MOTOR_SetSpeed(RIGHT, speedDroite * 1);
@@ -54,19 +55,63 @@ void Acceleration()
 void Decceleration()
 {
     delay(50);
-    MOTOR_SetSpeed(LEFT, speedGauche * 0.7);
-    MOTOR_SetSpeed(RIGHT, speedDroite * 0.7);
-    delay(50);
+    MOTOR_SetSpeed(LEFT, speedGauche * 0.6);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.6);
+    delay(1000);
+    /*delay(50);
     MOTOR_SetSpeed(LEFT, speedGauche * 0.5);
     MOTOR_SetSpeed(RIGHT, speedDroite * 0.5);
     delay(60);
+    
     MOTOR_SetSpeed(LEFT, speedGauche * 0.3);
     MOTOR_SetSpeed(RIGHT, speedDroite * 0.3);
-    delay(60);
+    delay(60);*/
     MOTOR_SetSpeed(LEFT, 0);
     MOTOR_SetSpeed(RIGHT, 0);
 }
 
+void PIDParMoteur()
+{ 
+    int comptePulseG = 0;
+    int comptePulseD = 0;
+    int comptePulseGTot = 0;
+    int comptePulseDTot = 0;
+
+    float compteErreurDTot = 0;
+    float compteErreurGTot = 0;
+    float compteErreurD = 0;
+    float compteErreurG = 0;
+
+    int comptePulseTheoCycle= 0 ;
+     
+    // 1 seconde
+    
+    comptePulseD = ENCODER_Read(RIGHT) - (nbCycle*3200);
+    comptePulseG = ENCODER_Read(LEFT) - (nbCycle*3200);
+    
+    comptePulseDTot+= comptePulseD;
+    comptePulseGTot+= comptePulseG;
+
+    delay(10);
+
+    compteErreurD = comptePulseTheo - comptePulseD;
+    compteErreurG = comptePulseTheo - comptePulseG;
+
+    comptePulseTheoCycle = nbCycle*comptePulseTheo;
+
+
+    compteErreurDTot = comptePulseTheoCycle - comptePulseDTot;
+    speedDroite+= (compteErreurDTot * 0.00002)+(compteErreurD * 0.0001) ;
+    
+    compteErreurGTot = comptePulseTheoCycle - comptePulseGTot;
+    speedGauche+= (compteErreurGTot * 0.00002)+(compteErreurG * 0.0001) ;
+ 
+
+    nbCycle++;
+   // ENCODER_Reset(RIGHT);
+    // ENCODER_Reset(LEFT);
+
+}
 void PIDBasic()
 {
     
@@ -120,25 +165,32 @@ void Avancer_Cm(int distance)
     speedGauche = 0.625;
     speedDroite = 0.6;
     Acceleration();
+    nbCycle = 0;
     int temps = 0;
+    int prochainCycle= 3200;
+
     while (Compteur < ComptePulse)
     {
-        if(temps >= 500)
+       
+       if(Compteur >= prochainCycle)
         {
-            nbCycle ++;
-            PIDBasic();
+            prochainCycle = Compteur + 3200;
+            PIDParMoteur();
             temps = 0;
         }
-        else
-            temps +=5;
+        
+        
+        delay(5);           
 
-        delay(5);    
         Compteur = ENCODER_Read(RIGHT);
        //PIDBasic();
     }
-    Decceleration();
+   
+    delay(50);
+    MOTOR_SetSpeed(LEFT, speedGauche * 0.6);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.6);
+    delay(1000);
 
-    delay(200);
 }
 
 void Tourne45(int direction)
@@ -231,10 +283,15 @@ void loop()
        int comptePulse = 0;
   // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
    
-    delay(2000);
-    
+    delay(1500);
+    /*
     Avancer_Cm(200);
-    delay(delayEntreMouvement);
+    delay(200);
+    Avancer_Cm(200);
+    while(1);
+    /**/
+    Avancer_Cm(100);
+    delay(500);
     //DEBUT DU U
     
     Tourne90(LEFT);
@@ -256,7 +313,7 @@ void loop()
     //FIN DU U 
     delay(delayEntreMouvement);
 
-   
+   /*
     ENCODER_Reset(LEFT);
     ENCODER_Reset(RIGHT);
         MOTOR_SetSpeed(RIGHT, 0.6);
