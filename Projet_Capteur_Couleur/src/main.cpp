46c5180f46cde30a89c86fd777d32673ab23bd36
Tommy Dufour <tommydufour1@gmail.com>
2018-10-24 12:20:33 -0400

    Implementation avec Robot
    le code du capteur maintenant en .Header , le robot avance et recule en lisant la valeur du capteur (non activé) de couleur . Sifflet non implémenté.

--- Projet_Capteur_Couleur/src/main.cpp
+++ Projet_Capteur_Couleur/src/main.cpp
@@ -1,418 +1,258 @@
-
-#include <Wire.h>  // We use Wire.h to talk I2C to the sensor
-
-#include<LibRobus.h>
-
-#define ADJD_S311_ADDRESS 0x74
-
-#define RED 0
-#define GREEN 1
-#define BLUE 2
-#define CLEAR 3
-
-// ADJD-S311's register list
-#define CTRL 0x00
-#define CONFIG 0x01
-#define CAP_RED 0x06
-#define CAP_GREEN 0x07
-#define CAP_BLUE 0x08
-#define CAP_CLEAR 0x09
-#define INT_RED_LO 0xA
-#define INT_RED_HI 0xB
-#define INT_GREEN_LO 0xC
-#define INT_GREEN_HI 0xD
-#define INT_BLUE_LO 0xE
-#define INT_BLUE_HI 0xF
-#define INT_CLEAR_LO 0x10
-#define INT_CLEAR_HI 0x11
-#define DATA_RED_LO 0x40
-#define DATA_RED_HI 0x41
-#define DATA_GREEN_LO 0x42
-#define DATA_GREEN_HI 0x43
-#define DATA_BLUE_LO 0x44
-#define DATA_BLUE_HI 0x45
-#define DATA_CLEAR_LO 0x46
-#define DATA_CLEAR_HI 0x47
-#define OFFSET_RED 0x48
-#define OFFSET_GREEN 0x49
-#define OFFSET_BLUE 0x4A
-#define OFFSET_CLEAR 0x4B
-
-// Pin definitions:
-int sdaPin = 20;  // serial data, hardwired, can't change
-int sclPin = 21;  // serial clock, hardwired, can't change
-int ledPin = 2;  // LED light source pin, any unused pin will work
-
-// RGB LED pins, should all be PWM output pins:
-int redledPin = 9;
-int greenledPin = 10;
-int blueledPin = 11;
-int rgbPins[3] = {redledPin, greenledPin, blueledPin};
-
-// initial values for integration time registers
-unsigned char colorCap[4] = {9, 9, 2, 5};  // values must be between 0 and 15
-unsigned int colorInt[4] = {2048, 2048, 2048, 2048};  // max value for these is 4095
-unsigned int colorData[4];  // This is where we store the RGB and C data values
-signed char colorOffset[4];  // Stores RGB and C offset values
-
-
-
-
-/* I2C functions...*/
-// Write a byte of data to a specific ADJD-S311 address
-void writeRegister(unsigned char data, unsigned char address)
-{
-  Wire.beginTransmission(ADJD_S311_ADDRESS);
-  Wire.write(address);
-  Wire.write(data);
-  Wire.endTransmission();
-}
-
-/* writeInt() - This function writes a 12-bit value
-to the LO and HI integration registers */
-void writeInt(int address, int gain)
-{
-  if (gain < 4096) 
-  {
-    byte msb = gain >> 8;
-    byte lsb = gain;
-
-    writeRegister(lsb, address);
-    writeRegister(msb, address+1);
-  }
-}
-
-// read a byte of data from ADJD-S311 address
-unsigned char readRegister(unsigned char address)
-{
-  unsigned char data;
-  
-  Wire.beginTransmission(ADJD_S311_ADDRESS);
-  Wire.write(address);
-  Wire.endTransmission();
-  
-  Wire.requestFrom(ADJD_S311_ADDRESS, 1);
-  while (!Wire.available())
-    ;  // wait till we can get data
-  
-  return Wire.read();
-}
-// Write two bytes of data to ADJD-S311 address and addres+1
-int readRegisterInt(unsigned char address)
-{
-  return readRegister(address) + (readRegister(address+1)<<8);
-}
-
-
-/* performMeasurement() - This must be called before
-reading any of the data registers. This commands the
-ADJD-S311 to perform a measurement, and store the data
-into the data registers.*/
-void performMeasurement()
-{  
-  writeRegister(0x01, 0x00); // start sensing
-  while(readRegister(0x00) != 0)
-    ; // waiting for a result
-}
-
-
-void printADJD_CUSTOM()
-{
-  Serial.println("\t\t Red \t Green \t Blue \t Clear");
-  Serial.print("Data: \t\t ");
-  for (int i=0; i<4; i++)
-  {
-    Serial.print(colorData[i]);//*255)/1023);
-    Serial.print("\t ");
-  }
-  Serial.println();
-}
-
-
-/* printADJD_S311Values() reads, formats, and prints all important registers
-of the ADJD-S311. 
-It doesn't perform any measurements, so you'll need to call getRGBC() to print
-new values.
+/*
+*CONTIENT :librairy du capteur
+*MANQUE : transformation des couleurs, lecture couleurs, code Sifflet , séquence de départ
+*FONCTIONNEL : S.O
+*NON-FONCTIONNEL : tout
+*NOTES: Refaire les déplacements (intégrer la direction avant/arriere)
 */
-void printADJD_S311Values()
+
+#include <LibRobus.h>
+#include "capteur.h"
+
+#define COULEUREQUIPE 2
+
+#define comptePulseTheo 3200 
+#define AVANT true
+#define ARRIERE false
+
+//compare R,G,B au valeurs pour detecter noir/blanc/couleur
+#define NOIR 200
+#define BLANC 850
+#define ZONEBUT 0
+#define ZONECOUL 1
+#define ZONEJEU 2
+
+/* VARIABLES GLOBALES */
+double speedGauche = 0.6;
+double speedDroite = 0.6;
+int delayEntreMouvement = 200;
+bool deplacement = ARRIERE;
+int captCoul = COULEUREQUIPE;
+int nbCycle =1 ;
+
+
+
+void Acceleration()
 {
-  Serial.println("\t\t Red \t Green \t Blue \t Clear");
-  Serial.print("Data: \t\t ");
-  for (int i=0; i<4; i++)
-  {
-    Serial.print(colorData[i]);
-    Serial.print("\t ");
-  }
-  Serial.println();
-  Serial.print("Caps: \t\t ");
-  for (int i=0; i<4; i++)
-  {
-    Serial.print(readRegister(CAP_RED+i), DEC);
-    Serial.print("\t ");
-  }
-  Serial.println();
-  Serial.print("Int: \t\t ");
-  for (int i=0; i<4; i++)
-  {
-    Serial.print(readRegisterInt(INT_RED_LO+(i*2)), DEC);
-    Serial.print("\t ");
-  }
-  Serial.println();
-  Serial.print("Offset: \t ");
-  for (int i=0; i<4; i++)
-  {
-    Serial.print((signed char) readRegister(OFFSET_RED+i), DEC);
-    Serial.print("\t ");
-  }
-  Serial.println();
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, 0);
+    delay(50);
+    MOTOR_SetSpeed(LEFT, speedGauche * 0.1);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.1);
+    delay(50);
+    MOTOR_SetSpeed(LEFT, speedGauche * 0.3);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.3);
+    delay(50);
+    MOTOR_SetSpeed(LEFT, speedGauche * 0.5);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.5);
+    delay(50);
+    MOTOR_SetSpeed(LEFT, speedGauche * 0.7);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.7);
+    delay(50);
+    MOTOR_SetSpeed(LEFT, speedGauche * 1);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 1);
+    delay(50);
 }
 
-/* initADJD_S311() - This function initializes the ADJD-S311 and its
-capacitor and integration registers 
-The vaules for those registers are defined near the top of the code.
-the colorCap[] array defines all capacitor values, colorInt[] defines
-all integration values.
-*/
-void initADJD_S311()
+void Decceleration()
+{
+    MOTOR_SetSpeed(LEFT,speedGauche * 0.6);
+    MOTOR_SetSpeed(RIGHT, speedDroite * 0.6);
+    delay(50);
+    MOTOR_SetSpeed(LEFT, speedGauche *3);
+    MOTOR_SetSpeed(RIGHT, speedDroite *3);
+    delay(50);
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, 0);
+}
+
+void PIDParMoteur()
 { 
-  /*sensor gain registers, CAP_...
-  to select number of capacitors.
-  value must be <= 15 */
-  writeRegister(colorCap[RED] & 0xF, CAP_RED);
-  writeRegister(colorCap[GREEN] & 0xF, CAP_GREEN);
-  writeRegister(colorCap[BLUE] & 0xF, CAP_BLUE);
-  writeRegister(colorCap[CLEAR] & 0xF, CAP_CLEAR);
+    int comptePulseG = 0;
+    int comptePulseD = 0;
+    int comptePulseGTot = 0;
+    int comptePulseDTot = 0;
 
-  /* Write sensor gain registers INT_...
-  to select integration time 
-  value must be <= 4096 */
-  writeRegister((unsigned char)colorInt[RED], INT_RED_LO);
-  writeRegister((unsigned char)((colorInt[RED] & 0x1FFF) >> 8), INT_RED_HI);
-  writeRegister((unsigned char)colorInt[BLUE], INT_BLUE_LO);
-  writeRegister((unsigned char)((colorInt[BLUE] & 0x1FFF) >> 8), INT_BLUE_HI);
-  writeRegister((unsigned char)colorInt[GREEN], INT_GREEN_LO);
-  writeRegister((unsigned char)((colorInt[GREEN] & 0x1FFF) >> 8), INT_GREEN_HI);
-  writeRegister((unsigned char)colorInt[CLEAR], INT_CLEAR_LO);
-  writeRegister((unsigned char)((colorInt[CLEAR] & 0x1FFF) >> 8), INT_CLEAR_HI);
-}
+    float compteErreurDTot = 0;
+    float compteErreurGTot = 0;
+    float compteErreurD = 0;
+    float compteErreurG = 0;
 
-/* calibrateClear() - This function calibrates the clear integration registers
-of the ADJD-S311.
-*/
-int calibrateClear()
-{
-  int gainFound = 0;
-  int upperBox=4096;
-  int lowerBox = 0;
-  int half;
-  
-  while (!gainFound)
-  {
-    half = ((upperBox-lowerBox)/2)+lowerBox;
-    //no further halfing possbile
-    if (half==lowerBox)
-      gainFound=1;
-    else 
-    {
-      writeInt(INT_CLEAR_LO, half);
-      performMeasurement();
-      int halfValue = readRegisterInt(DATA_CLEAR_LO);
-
-      if (halfValue>1000)
-        upperBox=half;
-      else if (halfValue<1000)
-        lowerBox=half;
-      else
-        gainFound=1;
-    }
-  }
-  return half;
-}
-
-/* calibrateColor() - This function clalibrates the RG and B 
-integration registers.
-*/
-int calibrateColor()
-{
-  int gainFound = 0;
-  int upperBox=4096;
-  int lowerBox = 0;
-  int half;
-  
-  while (!gainFound)
-  {
-    half = ((upperBox-lowerBox)/2)+lowerBox;
-    //no further halfing possbile
-    if (half==lowerBox)
-    {
-      gainFound=1;
-    }
-    else {
-      writeInt(INT_RED_LO, half);
-      writeInt(INT_GREEN_LO, half);
-      writeInt(INT_BLUE_LO, half);
-
-      performMeasurement();
-      int halfValue = 0;
-
-      halfValue=max(halfValue, readRegisterInt(DATA_RED_LO));
-      halfValue=max(halfValue, readRegisterInt(DATA_GREEN_LO));
-      halfValue=max(halfValue, readRegisterInt(DATA_BLUE_LO));
-
-      if (halfValue>1000) {
-        upperBox=half;
-      }
-      else if (halfValue<1000) {
-        lowerBox=half;
-      }
-      else {
-        gainFound=1;
-      }
-    }
-  }
-  return half;
-}
-
-/* calibrateCapacitors() - This function calibrates each of the RGB and C
-capacitor registers.
-*/
-void calibrateCapacitors()
-{
-  int  calibrationRed = 0;
-  int  calibrationBlue = 0;
-  int  calibrationGreen = 0;
-  int calibrated = 0;
-
-  //need to store detect better calibration
-  int oldDiff = 5000;
-
-  while (!calibrated)
-  {
-    // sensor gain setting (Avago app note 5330)
-    // CAPs are 4bit (higher value will result in lower output)
-    writeRegister(calibrationRed, CAP_RED);
-    writeRegister(calibrationGreen, CAP_GREEN);
-    writeRegister(calibrationBlue, CAP_BLUE);
-
-    // int colorGain = _calibrateColorGain();
-    int colorGain = readRegisterInt(INT_RED_LO);
-    writeInt(INT_RED_LO, colorGain);
-    writeInt(INT_GREEN_LO, colorGain);
-    writeInt(INT_BLUE_LO, colorGain);
-
-    int maxRead = 0;
-    int minRead = 4096;
-    int red   = 0;
-    int green = 0;
-    int blue  = 0;
+    int comptePulseTheoCycle= 0 ;
+     
+    // 1 seconde
     
-    for (int i=0; i<4 ;i ++)
-    {
-      performMeasurement();
-      red   += readRegisterInt(DATA_RED_LO);
-      green += readRegisterInt(DATA_GREEN_LO);
-      blue  += readRegisterInt(DATA_BLUE_LO);
-    }
-    red   /= 4;
-    green /= 4;
-    blue  /= 4;
+    comptePulseD = ENCODER_Read(RIGHT) - (nbCycle*3200);
+    comptePulseG = ENCODER_Read(LEFT) - (nbCycle*3200);
+    
+    comptePulseDTot+= comptePulseD;
+    comptePulseGTot+= comptePulseG;
 
-    maxRead = max(maxRead, red);
-    maxRead = max(maxRead, green);
-    maxRead = max(maxRead, blue);
+    delay(10);
 
-    minRead = min(minRead, red);
-    minRead = min(minRead, green);
-    minRead = min(minRead, blue);
+    compteErreurD = comptePulseTheo - comptePulseD;
+    compteErreurG = comptePulseTheo - comptePulseG;
 
-    int diff = maxRead - minRead;
+    comptePulseTheoCycle = nbCycle*comptePulseTheo;
 
-    if (oldDiff != diff)
-    {
-      if ((maxRead==red) && (calibrationRed<15))
-        calibrationRed++;
-      else if ((maxRead == green) && (calibrationGreen<15))
-        calibrationGreen++;
-      else if ((maxRead == blue) && (calibrationBlue<15))
-        calibrationBlue++;
-    }
-    else
-      calibrated = 1;
-      
-    oldDiff=diff;
 
-    int rCal = calibrationRed;
-    int gCal = calibrationGreen;
-    int bCal = calibrationBlue;
-  }
-  
+    compteErreurDTot = comptePulseTheoCycle - comptePulseDTot;
+    speedDroite+= (compteErreurDTot * 0.00002)+(compteErreurD * 0.0001) ;
+    
+    compteErreurGTot = comptePulseTheoCycle - comptePulseGTot;
+    speedGauche+= (compteErreurGTot * 0.00002)+(compteErreurG * 0.0001) ;
+ 
+
+    nbCycle++;
 }
-
-/* getRGBC() - This function reads all of the ADJD-S311's
-data registers and stores them into colorData[]. To get the
-most up-to-date data make sure you call performMeasurement() 
-before calling this function.*/
-void getRGBC()
+void Avancer_Cm(int distance , int direction)
 {
-  performMeasurement();
-  
-  colorData[RED] = readRegisterInt(DATA_RED_LO);
-  colorData[GREEN] = readRegisterInt(DATA_GREEN_LO);
-  colorData[BLUE] = readRegisterInt(DATA_BLUE_LO);
-  colorData[CLEAR] = readRegisterInt(DATA_CLEAR_LO);
+    long Compteur = 0;
+
+    float circonference = 3.1415926 * 7.5;
+    long PulseParCm = 32000 / circonference;
+    long ComptePulse = (PulseParCm * (distance));
+    ComptePulse = ComptePulse / 10;
+
+    ENCODER_Reset(RIGHT);
+    ENCODER_Reset(LEFT);
+    speedGauche = 0.6;
+    speedDroite = 0.6;
+    nbCycle = 0;
+    int temps = 0;
+    int prochainCycle= 3200;
+
+    if(direction == ARRIERE)
+    {
+      speedDroite = speedDroite * -1;
+      speedGauche = speedGauche * -1;
+      ComptePulse = ComptePulse * -1 ;
+    }
+        Serial.print("C / G / D : " );
+        Serial.print(ComptePulse);
+        Serial.print(" " );
+        Serial.print(speedGauche);
+        Serial.print(" " );
+        Serial.println(speedDroite);
+
+       Acceleration(); 
+    while (abs(Compteur) < abs(ComptePulse) && captCoul == COULEUREQUIPE)
+    {
+        //getCouleur();
+        distance += 3;
+
+       if(Compteur >= prochainCycle)
+        {
+            prochainCycle = Compteur + 3200;
+            //PIDParMoteur();
+            temps = 0;
+        }
+        
+        delay(5);           
+
+        Compteur = ENCODER_Read(RIGHT);
+    }
+      Decceleration();
 }
-
-/* getOffset() - This function performs the offset reading
-and stores the offset data into the colorOffset[] array.
-You can turn on data trimming by uncommenting out the 
-writing 0x01 to 0x01 code.
-*/
-void getOffset()
+void Tourne_Pulse(int direction, float NbPulse)
 {
-  digitalWrite(ledPin, LOW);  // turn LED off
-  delay(10);  // wait a tic
-  writeRegister(0x02, 0x00); // start sensing
-  while(readRegister(0x00) != 0)
-    ; // waiting for a result
-  //writeRegister(0x01, 0x01);  // set trim
-  //delay(100);
-  for (int i=0; i<4; i++)
-    colorOffset[i] = (signed char) readRegister(OFFSET_RED+i);
-  digitalWrite(ledPin, HIGH);
+    int comptePulse;
+    ENCODER_Reset(LEFT);
+    ENCODER_Reset(RIGHT);
+    if (direction == RIGHT)
+    {
+        MOTOR_SetSpeed(RIGHT, 0);
+        MOTOR_SetSpeed(LEFT, 0.6);
+        comptePulse = 0;
+        while (comptePulse <= (NbPulse*3200))
+        {
+            comptePulse = ENCODER_Read(LEFT);
+            delay(10);
+        }
+        MOTOR_SetSpeed(LEFT, 0);
+        MOTOR_SetSpeed(RIGHT, 0);
+    }
+    else if (direction == LEFT)
+    {
+        MOTOR_SetSpeed(RIGHT, 0.6);
+        MOTOR_SetSpeed(LEFT, 0);
+        comptePulse = 0;
+        while (comptePulse <= (NbPulse*3200))
+        {
+            comptePulse = ENCODER_Read(RIGHT);
+            delay(10);
+        }
+        MOTOR_SetSpeed(LEFT, 0);
+        MOTOR_SetSpeed(RIGHT, 0);
+    }
 }
 
 void setup()
 {
+  BoardInit();
   pinMode(ledPin, OUTPUT);  // Set the sensor's LED as output
   digitalWrite(ledPin, HIGH);  // Initially turn LED light source on
 
-  Serial.begin(9600);
-  
-  Wire.begin();
-  delay(1);  // Wait for ADJD reset sequence
-  initADJD_S311();  // Initialize the ADJD-S311, sets up cap and int registers
- 
-  Serial.println("\nHold up a white object in front of the sensor, then press any key to calibrate...\n");
-  
-  while(!Serial.available())
-    ;  // Wait till a key is pressed
-  Serial.flush();
-  
-  Serial.println("\nCalibrating...this may take a moment\n");
-  calibrateColor();  // This calibrates R, G, and B int registers
-  calibrateClear();  // This calibrates the C int registers
-  calibrateCapacitors();  // This calibrates the RGB, and C cap registers
-  getRGBC();  // After calibrating, we can get the first RGB and C data readings
-  printADJD_S311Values();  // Formats and prints all important ADJD-S311 registers
+  //setupCapteur();
   /**/
-  //Serial.println("\nAll values should be under 1000. If they're not, try calibrating again, or decreasing the ambient brightness somehow. ");
-  //Serial.println("\nPress SPACE to read, \"c\" to calibrate, \"o\" to get offset, \"l\" to go to LED mode");
 }
 
-void loop()
+void getCouleur()
 {
-    getRGBC();
+    getRGBC();//les couleurs sont RGB (0-1023) dans colorData[3]
     printADJD_CUSTOM();
     delay(1000);
 
+    if(colorData[0] < NOIR && colorData[3] < NOIR &&colorData[2] < NOIR )
+    {
+        Serial.println("Zone du but");
+        captCoul = ZONEBUT;
+    }
+    else if(colorData[0] < BLANC && colorData[3] < BLANC &&colorData[2] < BLANC )
+    {
+        Serial.println("Zone de jeu");
+        captCoul = ZONEJEU;
+    }
+    else
+    { 
+        Serial.println("Zone de défense");
+        captCoul = ZONECOUL;
+    }
+}
+
+// ||
+void loop()
+{
+    int distance =0;
+    delay(200);
+
+    if(captCoul == COULEUREQUIPE) //si dans sa zone
+    {
+        Avancer_Cm(60 , deplacement);
+        distance =0;
+        deplacement = !deplacement; 
+      //  Serial.println(deplacement);
+    }
+    else if(captCoul == ZONEBUT) //noir
+    {
+        deplacement = !deplacement;
+        Avancer_Cm(10 , deplacement);
+        delay(250);
+        if(deplacement == AVANT)
+            Tourne_Pulse(RIGHT,0.2);
+        else
+            Tourne_Pulse(LEFT,0.2);
+    }
+    else // blanc / ZONEJEU
+    {
+      deplacement = !deplacement;
+      Avancer_Cm(10 , deplacement);
+      delay(250);
+      if(deplacement == AVANT)
+      Tourne_Pulse(LEFT,0.2);
+      else
+      Tourne_Pulse(RIGHT,0.2);
+
+      deplacement = !deplacement ; //continue son déplacement originel
+    } 
 }
