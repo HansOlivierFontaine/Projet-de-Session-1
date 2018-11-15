6f6b0b971b7861fb18f9fc8523e3c23e9102fa0c
Tommy Dufour <tommydufour1@gmail.com>
2018-11-15 10:50:03 -0500

    Ajout FASTLED

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -11,6 +11,7 @@ Inclure les librairies de functions que vous voulez utiliser
 **************************************************************************** */
 
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
+#include <fastLED.h>
 /* ****************************************************************************
 Variables globales et defines
 **************************************************************************** */
@@ -34,7 +35,7 @@ void Acceleration()
 
         MOTOR_SetSpeed(RIGHT, speedDroite * pourcentage);
         MOTOR_SetSpeed(LEFT, speedGauche * pourcentage);
-        delay(30);
+        delay(10);
     }
 }
 
@@ -46,7 +47,7 @@ void Decceleration()
 
         MOTOR_SetSpeed(RIGHT, speedDroite * pourcentage);
         MOTOR_SetSpeed(LEFT, speedGauche * pourcentage);
-        delay(30);
+        delay(5);
     }
 }
 
@@ -59,7 +60,7 @@ void Avancer_Cm(int distance)
 {
     long compteur = 0;
 
-    float circonference = 3.1415926 * 7.5;
+    float circonference = PI * 7.5;
     long pulseParCm = 32000 / circonference;
     long comptePulse = (pulseParCm * (distance));
     comptePulse = comptePulse / 10;
@@ -80,11 +81,12 @@ void Avancer_Cm(int distance)
     delay(200);
 }
 
+
 void Reculer_Cm(int distance)
 {
     long compteur = 0;
 
-    float circonference = 3.1415926 * 7.5;
+    float circonference = PI * 7.5;
     long pulseParCm = 32000 / circonference;
     long comptePulse = (pulseParCm * (distance));
     comptePulse = comptePulse / 10;
@@ -95,7 +97,7 @@ void Reculer_Cm(int distance)
     speedGauche = -0.42;
     speedDroite = -0.4;
     Acceleration();
-    while (compteur < comptePulse)
+    while (abs(compteur) < comptePulse)
     {
         delay(5);
         compteur = ENCODER_Read(RIGHT);
@@ -149,7 +151,7 @@ void Demi_Tour()
     ENCODER_Reset(LEFT);
     ENCODER_Reset(RIGHT);
 
-    MOTOR_SetSpeed(RIGHT, 0);
+    MOTOR_SetSpeed(RIGHT, -0.4);
     MOTOR_SetSpeed(LEFT, 0.4);
     comptePulse = 0;
     while (comptePulse <= (1.18 * 3200))
@@ -162,17 +164,18 @@ void Demi_Tour()
 
     ENCODER_Reset(LEFT);
     ENCODER_Reset(RIGHT);
-
-    MOTOR_SetSpeed(RIGHT, 0.4);
+/*
+    MOTOR_SetSpeed(RIGHT, -0.4);
     MOTOR_SetSpeed(LEFT, 0);
     comptePulse = 0;
-    while (comptePulse <= (1.18 * 3200))
+    while (abs(comptePulse) <= (1.18 * 3200))
     {
-        comptePulse = ENCODER_Read(LEFT);
+        comptePulse = ENCODER_Read(RIGHT);
         delay(10);
     }
     MOTOR_SetSpeed(LEFT, 0);
     MOTOR_SetSpeed(RIGHT, 0);
+    */
 }
 
 /* ****************************************************************************
@@ -182,13 +185,73 @@ Fonctions d'initialisation (setup)
 // -> Se fait appeler seulement un fois
 // -> Generalement on y initilise les varibbles globales
 
+#define LED_PIN     4
+#define NUM_LEDS    120
+#define BRIGHTNESS  100
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+void LEDSTRIP_Standby()
+{
+    memset(leds, 0, NUM_LEDS * 3);
+    for(int i = 0 ; i < NUM_LEDS; i++ ) {
+
+    //leds[i-1] = CRGB::Black;
+    leds[i] = CRGB::HotPink;
+    
+    FastLED.show();
+    delay(10);
+    }
+
+    for(int i =BRIGHTNESS; i > 0 ; i--)
+    {
+      FastLED.setBrightness( i );
+        FastLED.show();
+      delay(10);
+    }
+
+   FastLED.setBrightness(BRIGHTNESS);
+}
+
+void LEDSTRIP_Level(int level)
+{
+    int color;
+    if(level == 0)
+        color = CRGB::Green;
+    if(level == 1)
+        color = CRGB::Yellow;
+    if(level == 2)
+        color = CRGB::Red;
+
+      // 3 at a time
+    for(int i = 3 ; i < NUM_LEDS; i++ ) {
+      memset(leds, 0, NUM_LEDS * 3);
+       
+        leds[i] = color;
+        leds[i-1] = color;
+        leds[i-2] = color; 
+
+      FastLED.show();
+      delay(10);
+    }
+  
+}
+
+
+
 void setup()
 {
     BoardInit();
-    pinMode(LED_BUILTIN, OUTPUT);
+
+     delay( 3000 ); // power-up safety delay
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+    
+    /*pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
     SERVO_Enable(1);
-    SERVO_Enable(0);
+    SERVO_Enable(0);*/
 }
 
 /* ****************************************************************************
@@ -196,18 +259,27 @@ Fonctions de boucle infini (nloop())
 **************************************************************************** */
 // -> Se fait appeler perpetuellement suite au "setup"
 
+
+//https://www.tweaking4all.com/hardware/arduino/arduino-ws2812-led/
+
 void loop()
 {
-    SERVO_SetAngle(1, 180);
+
+    //LEDSTRIP_Standby();
+    LEDSTRIP_Level(0);
+    LEDSTRIP_Level(1);
+    LEDSTRIP_Level(2);
+
+    /*
+    delay(1000);
+    Tourne_Droite();
+    Tourne_Gauche();
     delay(500);
-    SERVO_SetAngle(0, 0);
-    delay(3000);
-    SERVO_SetAngle(1, 90);
-    delay(3000);
-    SERVO_SetAngle(0, 90);
-    delay(3000);
-    SERVO_SetAngle(1, 180);
-    delay(3000);
-    SERVO_SetAngle(0, 0);
-    delay(3000);
+    Demi_Tour();
+    delay(1000);
+    Avancer_Cm(15);
+    delay(1000);
+    Reculer_Cm(15);
+    delay(5000);
+*/
 }
